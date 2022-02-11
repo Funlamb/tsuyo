3b31f60e78c429d5df85f172451a0e4919b8528b
Funlamb <funlamb@gmail.com>
2022-02-10 19:35:58 -0800

    Added functionality to start page

--- start.py
+++ start.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
 
+def get_db_connection():
+   db = sqlite3.connect("workout.db")
+   db.row_factory = sqlite3.Row
+   return db
+
 @app.route('/')
-def hello_world():
-   return 'Hello Worlds'
+def index():
+   db = get_db_connection()
+   posts = db.execute("SELECT * FROM users").fetchall()
+   db.close()
+   return render_template("index.html", posts=posts)
 
 if __name__ == '__main__':
    app.run()
