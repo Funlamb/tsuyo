1e274b5d509bd440a38f590bc7e5eecc998981b3
Funlamb <funlamb@gmail.com>
2022-02-11 20:04:18 -0800

    Added ability to see all of mike's workout when clicking on Mike

--- start.py
+++ start.py
@@ -11,11 +11,29 @@ def get_db_connection():
 
 @app.route('/')
 def index():
+   print("Hello")
    db = get_db_connection()
    posts = db.execute("SELECT * FROM users").fetchall()
+   print(posts)
    db.close()
-   print("Hello")
    return render_template("index.html", posts=posts)
+
+@app.route('/mike')
+def mike():
+   db = get_db_connection()
+   print("Mike")
+   query = """SELECT users.firstName, workouts.dateandtime, sets.*, exercises.name FROM users
+      JOIN workouts ON users.id = workouts.userID JOIN sets ON workouts.id = sets.workoutID JOIN exercises ON
+      sets.exerciseID = exercises.id WHERE users.id = 1"""
+   # query = """SELECT * FROM workouts"""
+   posts = db.execute(query).fetchall()
+   print(posts[0][4])
+   db.close()
+   return render_template("mike.html", posts=posts)
+
+@app.route('/help')
+def help():
+   return "Helped"
 
 if __name__ == '__main__':
    app.run()
