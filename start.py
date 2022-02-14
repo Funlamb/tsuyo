0b275a338319f720fa083e2b780ce8d985601917
Funlamb <funlamb@gmail.com>
2022-02-14 09:20:25 -0800

    Trying to orginize data

--- start.py
+++ start.py
@@ -20,14 +20,29 @@ def index():
 @app.route('/mike')
 def mike():
    db = get_db_connection()
-   query = """SELECT users.firstName, workouts.dateandtime, sets.*, exercises.name FROM users
+   query = """SELECT users.firstName, workouts.dateandtime, workouts.id, sets.*, exercises.name FROM users
       JOIN workouts ON users.id = workouts.userID JOIN sets ON workouts.id = sets.workoutID JOIN exercises ON
       sets.exerciseID = exercises.id WHERE users.id = 1"""
    posts = db.execute(query).fetchall()
    # Get all workout orginized
    # Split them by days
    # Split them by exercises
-   # Give them to the .thml to sort nicly 
+   # Give them to the .html to sort nicly 
+   workoutID = 0
+   workoutID = posts[0]["id"]
+   user_posts = []
+   daily_posts = []
+   exercise_posts = []
+   temp = []
+   for p in posts:
+      if p['id'] == workoutID:
+         temp.append(p)
+      daily_posts.append(temp)
+      temp = []
+      workoutID = p['id']
+      temp.append(p)
+   print(daily_posts)
+   print(workoutID)
    db.close()
    return render_template("mike.html", posts=posts)
 
