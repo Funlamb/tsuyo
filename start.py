584487594203d4a12c33876af468aef1ac9e60b0
Funlamb <funlamb@gmail.com>
2022-02-15 12:44:51 -0800

    Fast

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+from pkgutil import extend_path
 from flask import Flask, redirect, render_template, request, session
 
 import sqlite3
@@ -13,7 +14,6 @@ def get_db_connection():
 def index():
    db = get_db_connection()
    posts = db.execute("SELECT * FROM users").fetchall()
-   print(posts)
    db.close()
    return render_template("index.html", posts=posts)
 
@@ -23,30 +23,51 @@ def mike():
    query = """SELECT users.firstName, workouts.dateandtime, workouts.id, sets.*, exercises.name FROM users
       JOIN workouts ON users.id = workouts.userID JOIN sets ON workouts.id = sets.workoutID JOIN exercises ON
       sets.exerciseID = exercises.id WHERE users.id = 1"""
-   posts = db.execute(query).fetchall()
+   posts_unsorted = db.execute(query).fetchall()
    
    # Get all workout orginized
-   daily_posts = []
+   posts_sorted_daily = []
    temp = []
    # Split them by days
-   workoutID = posts[0]["id"]
-   for p in posts:
+   workoutID = posts_unsorted[0]["id"]
+   for p in posts_unsorted:
       if p['id'] != workoutID:
-         daily_posts.append(temp)
+         posts_sorted_daily.append(temp)
          # print(daily_posts)
          temp = []
          workoutID = p['id']
       temp.append(p)   
-   daily_posts.append(temp) # Adds the last day from the query
+   posts_sorted_daily.append(temp) # Adds the last day from the query
    temp = []
-   print(daily_posts[0][0]["Dateandtime"])
-   # Split them by exercises
-   exercise_posts = []
    
+   # Split them by exercises
+   posts_sorted_exercise = []
+   # Take the posts_sorted_daily and sort them by exercise
+   # Look at the daily post
+   day_temp = []
+   exercise_temp = []
+   for daily in posts_sorted_daily:
+      exerciseID = daily[0]['exerciseID'] # get the first exerciseID
+      for exercise in daily:
+         if exercise['exerciseID'] != exerciseID:
+            day_temp.append(exercise_temp)
+            exercise_temp = []
+            exerciseID = exercise['exerciseID']
+         exercise_temp.append(exercise)
+      
+      posts_sorted_exercise.append(exercise_temp)
+   for day in posts_sorted_exercise:
+      for d in day:
+         print(d['dateandtime'])
+   # Find the exercise
+   # Put the exercise in a temp list
+   # Add the exercise list to new daily post list
+   # Add the daily post list to posts_sorted_exercise list
+
    # Give them to the .html to sort nicly 
 
    db.close()
-   return render_template("mike.html", posts=daily_posts)
+   return render_template("mike.html", posts=posts_sorted_daily)
 
 @app.route('/help')
 def help():
