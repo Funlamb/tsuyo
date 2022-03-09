845829aa3d8382bc4ab5e3bfec22eac667853e90
Funlamb <funlamb@gmail.com>
2022-03-09 15:36:33 -0800

    Got exercise.html add row button to disable and enable when needed

--- static/exjs.js
+++ static/exjs.js
@@ -1,3 +1,66 @@
-// function dome(){
-    alert("did me");
-// }
+function addRow(){
+    // Disable button
+    document.getElementById("addCap").disabled = true;
+    // Add a new row to exercises
+    let datetime = $("#datetime");
+    var input;
+    var input = $("<input>").attr("type", "datetime-local").attr("name", "ndatetime[]");  
+    var br = $("<br>");
+    datetime.append(br);
+    datetime.append(input);
+
+    let exercise = $("#exercise");
+    var input;
+    var input = $("<input>").attr("type", "text").attr("name", "nExercise[]");  
+    var br = $("<br>");
+    exercise.append(br);
+    exercise.append(input);
+
+    let setNumber = $("#setnumber");
+    var input;
+    var input = $("<input>").attr("type", "number").attr("name", "nsetnumber[]");  
+    var br = $("<br>");
+    setNumber.append(br);
+    setNumber.append(input);
+
+    let rep = $("#rep")
+    var input;
+    var input = $("<input>").attr("type", "number").attr("name", "nrep[]");  
+    var br = $("<br>");
+    rep.append(br);
+    rep.append(input);
+
+    let resistance = $("#resistance")
+    var input;
+    var input = $("<input>").attr("type", "number").attr("name", "nresistance[]").attr("step", ".5");  
+    var br = $("<br>");
+    resistance.append(br);
+    resistance.append(input);
+    addInputs();
+}
+
+function addInputs(){
+    let inputs = document.getElementsByTagName("input");
+    for (let index = 0; index < inputs.length; index++) {
+        inputs[index].addEventListener("change", checkFilled);
+    }
+}
+
+function checkFilled(){
+    // alert("cheakfilled()");
+    let inputs = document.getElementsByTagName("input");
+    let count = inputs.length;
+    let testCount = 0;
+    for (let index = 0; index < count; index++) {
+        if (inputs[index].value !== "") {
+            testCount += 1;
+            // alert(testCount);
+        }
+    }
+    if (testCount == count){
+        alert("All filled");
+        document.getElementById('addCap').removeAttribute("disabled");
+    }
+}
+
+addInputs();
