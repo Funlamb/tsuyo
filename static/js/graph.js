34e0f0be8fcba020d0607871bb34ced2b7fe357e
Funlamb <funlamb@gmail.com>
2022-04-18 14:02:43 -0700

    Cleaned up css files.

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,6 +1,18 @@
 let json;
 let obj;
+let exercise_name;
 
+let mainColor;
+let mainColorSide;
+let secondColor;
+let secondColorSide;
+
+function initColors(color1, alpha1, color2, alpha2, color3, alpha3, color4, alpha4){
+    mainColor = hexToRGB(color1, alpha1);
+    mainColorSide = hexToRGB(color2, alpha2);
+    secondColor = hexToRGB(color3, alpha3);
+    secondColorSide = hexToRGB(color4, alpha4);
+}
 // Get chart to update with drop down options
 async function updateChart(dropdown){
     const exercise = obj.exercises;
@@ -8,48 +20,91 @@ async function updateChart(dropdown){
     const date = exercise[dropdown.value].map(
         function(index){
             date_lst = index.workout_date;
-            console.log(date_lst);
             let date_modded = date_lst.replace("T", " ");
             return date_modded;
         }
     );
-    const data = exercise[dropdown.value].map(
+    const data_resistance = exercise[dropdown.value].map(
         function(index){
             return index.resistance;
         }
     );
+    const data_interval = exercise[dropdown.value].map(
+        function(index){
+            return index.interval;
+        }
+    );
     myChart.data.labels = date;
-    myChart.data.datasets[0].label = dropdown.value;
-    myChart.data.datasets[0].data = data;
-
+    myChart.data.datasets[0].data = data_resistance;
+    myChart.data.datasets[0].backgroundColor = [mainColor];
+    myChart.data.datasets[0].borderColor = [mainColorSide];
+    myChart.data.datasets[1].data = data_interval;
+    myChart.data.datasets[1].backgroundColor = [secondColor];
+    myChart.data.datasets[1].borderColor = [secondColorSide];
+    exercise_name.innerHTML = dropdown.value;
     myChart.update();
 }
 
-// setup 
+function hexToRGB(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+}
+
 let data = {
     labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     datasets: [{
-    label: 'Weekly Sales',
-    data: [18, 12, 6, 9, 12, 3, 9],
-    backgroundColor: [
-        'rgba(255, 26, 104, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(0, 0, 0, 0.2)'
-    ],
-    borderColor: [
-        'rgba(255, 26, 104, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-        'rgba(0, 0, 0, 1)'
-    ],
-    borderWidth: 1
+        label: 'Resistance',
+        data: [18, 12, 6, 9, 12, 3, 9],
+        backgroundColor: [
+            'rgba(255, 26, 104, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(0, 0, 0, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 26, 104, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(0, 0, 0, 1)'
+        ],
+        borderWidth: 1
+    },
+    {
+        type: 'bar',
+        label: 'Intervals',
+        data: [18, 12, 6, 9, 12, 3, 9],
+        backgroundColor: [
+            'rgba(255, 26, 104, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(0, 0, 0, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 26, 104, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(0, 0, 0, 1)'
+        ],
+        borderWidth: 1
     }]
 };
 
