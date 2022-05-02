d0c939e6959ec4737a5f0fa100984f6473c1584b
Funlamb <funlamb@gmail.com>
2022-05-02 14:53:14 -0700

    Fixed tab order and focus the exercise when adding sets

--- static/js/exercise.js
+++ static/js/exercise.js
@@ -8,42 +8,49 @@ function addRow(){
     let datetimeValue = document.getElementById("datetime").lastElementChild.value;
     let datetime = $("#datetime");
     var input;
-    var input = $("<input>").attr("type", "datetime-local").attr("name", dateTimeName).attr("value", datetimeValue);  
+    var input = $("<input>").attr("type", "datetime-local").attr("name", dateTimeName).attr("value", datetimeValue).attr("tabindex", tabIndexNum);  
     var br = $("<br>");
     datetime.append(br);
     datetime.append(input);
+    tabIndexNum++;
     
     let exerciseValue = document.getElementById("exercise").lastElementChild.value;
     let exercise = $("#exercise");
     var input;
-    var input = $("<input>").attr("type", "text").attr("name", exerciseName).attr("value", exerciseValue);  
+    var input = $("<input>").attr("type", "text").attr("name", exerciseName).attr("value", exerciseValue).attr("tabindex", tabIndexNum);  
     var br = $("<br>");
     exercise.append(br);
     exercise.append(input);
+    input.focus();
+    tabIndexNum++;
 
     let setNumberValue = parseInt(document.getElementById("setnumber").lastElementChild.value) + 1;
     let setNumber = $("#setnumber");
     var input;
-    var input = $("<input>").attr("type", "number").attr("name", setNumberName).attr("value", setNumberValue);  
+    var input = $("<input>").attr("type", "number").attr("name", setNumberName).attr("value", setNumberValue).attr("tabindex", tabIndexNum);  
     var br = $("<br>");
     setNumber.append(br);
     setNumber.append(input);
+    tabIndexNum++;
 
     let repValue = parseInt(document.getElementById("rep").lastElementChild.value);
     let rep = $("#rep")
     var input;
-    var input = $("<input>").attr("type", "number").attr("name", repName).attr("value", repValue);  
+    var input = $("<input>").attr("type", "number").attr("name", repName).attr("value", repValue).attr("tabindex", tabIndexNum);  
     var br = $("<br>");
     rep.append(br);
     rep.append(input);
+    tabIndexNum++;
 
     let resistanceValue = parseInt(document.getElementById("resistance").lastElementChild.value);
     let resistance = $("#resistance")
     var input;
-    var input = $("<input>").attr("type", "number").attr("name", resistanceName).attr("step", ".5").attr("value", resistanceValue);  
+    var input = $("<input>").attr("type", "number").attr("name", resistanceName).attr("step", ".5").attr("value", resistanceValue).attr("tabindex", tabIndexNum);  
     var br = $("<br>");
     resistance.append(br);
     resistance.append(input);
+    tabIndexNum++;
+
 }
 
 function trackInputs(){
@@ -67,41 +74,48 @@ function checkFilled(){
     }
 }
 
+let tabIndexNum = 1;
 function addInitialRow(){
     let datetime = $("#datetime");
     var input;
-    var input = $("<input>").attr("type", "datetime-local").attr("name", dateTimeName);  
+    var input = $("<input>").attr("type", "datetime-local").attr("name", dateTimeName).attr("tabindex", tabIndexNum);  
     var br = $("<br>");
     datetime.append(br);
     datetime.append(input);
-    
+    tabIndexNum++;
+
     let exercise = $("#exercise");
     var input;
-    var input = $("<input>").attr("type", "text").attr("name", exerciseName);  
+    var input = $("<input>").attr("type", "text").attr("name", exerciseName).attr("tabindex", tabIndexNum);  
     var br = $("<br>");
     exercise.append(br);
     exercise.append(input);
+    tabIndexNum++;
 
     let setNumber = $("#setnumber");
     var input;
-    var input = $("<input>").attr("type", "number").attr("name", setNumberName);
+    var input = $("<input>").attr("type", "number").attr("name", setNumberName).attr("tabindex", tabIndexNum);
     var br = $("<br>");
     setNumber.append(br);
     setNumber.append(input);
+    tabIndexNum++;
 
     let rep = $("#rep")
     var input;
-    var input = $("<input>").attr("type", "number").attr("name", repName)
+    var input = $("<input>").attr("type", "number").attr("name", repName).attr("tabindex", tabIndexNum);
     var br = $("<br>");
     rep.append(br);
     rep.append(input);
+    tabIndexNum++;
 
     let resistance = $("#resistance")
     var input;
-    var input = $("<input>").attr("type", "number").attr("name", resistanceName).attr("step", ".5");
+    var input = $("<input>").attr("type", "number").attr("name", resistanceName).attr("step", ".5").attr("tabindex", tabIndexNum);
     var br = $("<br>");
     resistance.append(br);
     resistance.append(input);
+    tabIndexNum++;
+
     trackInputs();
 }
 
